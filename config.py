d3deb260d749022cccd31d59d6343ada9a3eb215
jacma <jackma246@gmail.com>
2025-08-28 18:33:43 -0700

    config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here-change-this-in-production'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'jackrocks'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///poker_ledger.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'uploads'
